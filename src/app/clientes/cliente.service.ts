22b9f0139804f26633c6b591f57db34bb7f6519c
Yuzmhar Guillen <yuzmhar.guillen@elemetrica.com>
2019-04-21 18:03:46 -0400

    Conexión con backend

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente.js';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-
-  constructor() { }
+  private urlEndPoint = 'http://localhost:8080/api/clientes';
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
   }
 }
